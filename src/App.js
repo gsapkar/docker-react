b94c34770a99bcc91e55f0856f8e8b001c0a9438
gsapkar <gsapkar@gmail.com>
2020-01-16 23:50:33 +0100

    feature test fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on feature branhc
+          I was changed on feature branch to learn react
         </a>
       </header>
     </div>
